505e80e40d48e53490691bbae443fc1b5c634ee5
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-05-29 18:14:02 -0300

    fix: change reponse to void

--- Back-end/src/middlewares/handleError.middleware.ts
+++ Back-end/src/middlewares/handleError.middleware.ts
@@ -8,21 +8,21 @@ const handleError = (
   req: Request,
   res: Response,
   next: NextFunction
-): Response => {
+): void => {
   if (error instanceof AppError) {
-    return res.status(error.status).json({ message: error.message });
+    res.status(error.status).json({ message: error.message });
   }
 
   if (error instanceof z.ZodError) {
-    return res.status(400).json({ message: error.flatten().fieldErrors });
+    res.status(400).json({ message: error.flatten().fieldErrors });
   }
 
   if (error instanceof JsonWebTokenError) {
-    return res.status(401).json({ message: error.message });
+    res.status(401).json({ message: error.message });
   }
 
   console.error(error);
-  return res.status(500).json({ error: "Internal server error" });
+  res.status(500).json({ error: "Internal server error" });
 };
 
 export { handleError };
