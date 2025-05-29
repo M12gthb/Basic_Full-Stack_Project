5051a4150f2770b4dbaa8f8198c0e1e2c1daaa89
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-05-29 20:22:00 -0300

    fix: verify uuid type

--- Back-end/src/middlewares/verifyUserId.middleware.ts
+++ Back-end/src/middlewares/verifyUserId.middleware.ts
@@ -2,12 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../entities";
 import { usersRepository } from "../repositories";
 import { AppError } from "../error/App.error";
+import { validate as uuidValidate } from "uuid";
 
 export const verifyUserId = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
+  if (!uuidValidate(req.params.id)) {
+    throw new AppError("user not found", 404);
+  }
   const id: string = req.params.id;
 
   const foundEntity: User | null = await usersRepository.findOneBy({ id });
