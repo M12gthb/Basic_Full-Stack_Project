b0e1e47599e43f82c2ca9d22a8b0f01193ff0565
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-05-29 17:28:28 -0300

    feat: add schemas

---
+++ Back-end/src/schemas/users.schema.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+const userSchema = z.object({
+  id: z.number().positive(),
+  name: z.string().max(45),
+  email: z.string().max(45).email(),
+  password: z.string().max(120),
+  createdAt: z.string().or(z.date()),
+  updatedAt: z.string().or(z.date()),
+  deletedAt: z.string().or(z.date()).nullable(),
+});
+
+const createUserSchema = userSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  deletedAt: true,
+});
+
+const userReturnSchema = userSchema.omit({ password: true });
+
+const userReadSchema = userReturnSchema.array();
+
+const patchUserSchema = userSchema
+  .partial({ name: true, email: true, password: true })
+  .omit({
+    id: true,
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+  });
+
+export {
+  userSchema,
+  userReturnSchema,
+  patchUserSchema,
+  createUserSchema,
+  userReadSchema,
+};
