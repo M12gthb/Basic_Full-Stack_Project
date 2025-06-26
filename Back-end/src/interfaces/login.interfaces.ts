506943164fab6700703d7a73c2322efa209f1f95
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-06-26 15:14:35 -0300

    fix: add id to login response

--- Back-end/src/interfaces/login.interfaces.ts
+++ Back-end/src/interfaces/login.interfaces.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 import { loginSchema } from "../schemas/login.schema";
 
 type loginCreate = z.infer<typeof loginSchema>;
-type loginReturn = { token: string };
+type loginReturn = { token: string; id: string };
 
 export { loginReturn, loginCreate };
--- Back-end/src/services/login.services.ts
+++ Back-end/src/services/login.services.ts
@@ -57,6 +57,7 @@ const loginServices = async ({
 
   return {
     token,
+    id: foundUser.id,
   };
 };
 
